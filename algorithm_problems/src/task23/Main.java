41d1e6339488663ba5cde8934a949ded261bbda0
mnegal <maxim.negaliuc@endava.com>
2019-03-06 11:59:58 +0200

    asserts for 23

--- algorithm_problems/src/task23/Main.java
+++ algorithm_problems/src/task23/Main.java
@@ -1,5 +1,8 @@
 package task23;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /*
 Given an array of n integers, determine the minimum amount of integers
 to be removed from the array so that the remaining array forms an ascending sequence
@@ -9,16 +12,23 @@ Given input array:
 The following output should be displayed:
 2 2 2 2 2 3 3 3 7
 */
+
 public class Main {
+    @Test
     public static void main(String[] args) {
         int[] a = {2, 5, 3, 4, 2, 2, 2, 7, 8, 2, 3, 3, 3, 7};
         int[] b = {1, 1, 2, 1, 2, 2, 3, 5, 3, 1, 4, 4, 4};
-        int[] res = new int[0];
+        int[] resA = new int[0];
+        int[] resB = new int[0];
         try {
-            res = new Solution().removeIt(a);
+            resA = new Solution().removeIt(a);
+            resB = new Solution().removeIt(b);
         } catch (CustomExceptions customExceptions) {
             customExceptions.printStackTrace();
         }
-        for (int i : res) System.out.print(i + "\t");
+
+        Assert.assertArrayEquals(new int[]{2, 2, 2, 2, 2, 3, 3, 3, 7}, resA);
+        Assert.assertArrayEquals(new int[]{1, 1, 1, 2, 2, 3, 3, 4, 4, 4}, resB);
+
     }
 }
